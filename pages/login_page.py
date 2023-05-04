c69a5e60d07af1a9cdb4d10a394bda2ad8009534
MemnumOldum <127943396+MemnumOldum@users.noreply.github.com>
2023-05-04 16:07:19 +0400

    Add files via upload

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+# класс LoginPage - наследник класса BasePage
+# (класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка)
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert 'login' in current_url, 'It is not login page'
+        assert True
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert True
---
+++ pages/main_page.py
@@ -0,0 +1,16 @@
+#  импорт базового класса BasePage
+from .base_page import BasePage
+from .locators import MainPageLocators
+from .login_page import LoginPage
+
+
+# класс MainPage - наследник класса BasePage
+# (класс MainPage будет иметь доступ ко всем атрибутам и методам своего класса-предка)
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
